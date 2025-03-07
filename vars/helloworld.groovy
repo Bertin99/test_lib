a0909188feaec78ddca4f5fece519dbd1905200f
bertino <bertin99gadjou.com>
2025-03-07 11:19:20 +0100

    deux

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,5 +1,16 @@
 #!/usr/bin/env groovy
 import groovy.json.*
- def call (body) {
-  println(" hello word !!!")
+def call (body) {
+ def map vars    = [ : ]
+ boby.resolveStrategy		= closure.DELEGATE_FIRST
+ boby.delegate                  = mapVars
+ boby ()
+
+ def name = maVars.name
+ def action = maVars.action
+ println( action + " " + name + " !! ")
+
+
+
+
 }
